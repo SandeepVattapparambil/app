d58dab9a51431866e0d74d8ce8edfc6c2eb570dc
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-07-10 12:15:50 +0530

    edits

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,14 +1,20 @@
 <?php
+  //Set name of Database host
   $db_host_name = "localhost";
+  //Set username for Database host
   $db_host_username = "root";
+  //Set password for Database host
   $db_host_password = "";
+  //Set name for Database
   $db_name = "nas_media_server";
   // Create connection
   $conn = mysqli_connect($db_host_name, $db_host_username, $db_host_password, $db_name);
   // Check connection
-  if (!$conn) {
-      die("\nConnection failed: " . mysqli_connect_error());
+  if(!$conn){
+    //Show error if not connected
+    die("\nConnection failed: " . mysqli_connect_error());
   }
-    echo "\nConnected successfully";
-    //mysqli_close($conn);
+  //Show message if connection successfull
+  echo "\nConnected successfully";
+  //mysqli_close($conn);
 ?>
--- php/login.php
+++ php/login.php
@@ -1,17 +1,24 @@
 <?php
+//Require Database connection configuration file
 require_once('db_connection.php');
+//Check whether the form data fields are set
 if(isset($_POST['username']) && isset($_POST['password'])){
+  //Assign values to variables
   $user_name = $_POST['username'];
   $password = $_POST['password'];
-  $user_record = get_user_record($user_name, $password, $conn, $db_name);
-  //login($user_name, $password, $conn, $db_name);
+  //Get user record based on form data
+  $user_record = get_user_record($user_name, $conn, $db_name);
+  //Check authentication
+  check_user($user_name, $password, $user_record);
 }
 else{
+  //User record not found
   echo "\nNo data found!";
 }
-function get_user_record($user_name, $password, $conn, $db_name){
+//function to get user record
+function get_user_record($user_name, $conn, $db_name){
   mysqli_select_db($conn, $db_name);
-  $sql = "SELECT * FROM user WHERE username='$user_name' AND password='$password'";
+  $sql = "SELECT * FROM user WHERE username='$user_name'";
   $result = mysqli_query($conn, $sql);//or die('Query:<br />' . $sql . '<br /><br />Error:<br />' . mysql_error());
   $count_rows = mysqli_num_rows($result);
   if($count_rows > 0){
@@ -23,12 +30,24 @@ function get_user_record($user_name, $password, $conn, $db_name){
       $password = $row['password'];
     }
     $user_record = array($id, $username, $password);
+    //echo count($user_record);
   }
   else{
+    $user_record = array(0,0,0);
     echo "\nRecord Not Found";
   }
   return $user_record;
 }
+//function to check user for authentication
+function check_user($user_name, $password, $user_record){
+  if($user_name != $user_record[1]){
+    echo "\nNo user found";
+  }
+  if($user_name == $user_record[1] && $password != $user_record[2]){
+    echo "\nUser found but password not a match";
+  }
+}
+
 function login($user_name, $password, $conn, $db_name){
   mysqli_select_db($conn, $db_name);
   $sql = "SELECT * FROM user WHERE username='$user_name' AND password='$password'";
