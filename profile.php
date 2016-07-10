c43ff3c1e0dd9db4e65711c67d08d767e4215a05
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-07-10 14:32:57 +0530

    updates

--- profile.php
+++ profile.php
@@ -61,20 +61,7 @@ if(!isset($_SESSION['id'])){
 </nav>
     <div class="container">
       <div class="row">
-        <div class="col-md-7 col-md-offset-2">
-          <div class="alert alert-success alert-dismissible fade in" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">×</span></button>
-     <div class="media">
-       <div class="media-left"> <a href="#">
-         <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="img/welcome.png" data-holder-rendered="true" style="width: 64px; height: 64px;"> </a> </div>
-         <div class="media-body">
-           <h4 class="media-heading">Welcome <?php echo $_SESSION['user_name'];?>!</h4>
-            Cras sit amet nibh libero, in e felis in faucibus.<a href="profile.php"> Edit Profile</a>
-         </div>
-       </div>
-        </div>
-      </div>
+        
       </div>
     </div>
 
