ba40b226c81d60ff3785f1f48703632d470a47b8
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-07-11 12:48:58 +0530

    updates success

--- php/profile_edit.php
+++ php/profile_edit.php
@@ -1,3 +1,29 @@
 <?php
-
+//Require Database connection configuration file
+require_once('db_connection.php');
+//Check whether the form data fields are set
+if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['id'])){
+  //Assign values to variables
+  $id = $_POST['id'];
+  $user_name = $_POST['username'];
+  $password = $_POST['password'];
+  update_user($id, $user_name, $password, $conn, $db_name);
+}
+else{
+  echo "\nNo data found!";
+}
+function update_user($id, $user_name, $password, $conn, $db_name){
+  echo $id. $user_name. $password;
+  mysqli_select_db($conn, $db_name);
+  $sql = "UPDATE user SET username = '$user_name', password = '$password' WHERE id = '$id'";
+  $result = mysqli_query($conn, $sql)or die('Query:<br />' . $sql . '<br /><br />Error:<br />' . mysql_error());
+  if($result){
+    echo "\nUpdate successful";
+    return;
+  }
+  else{
+    echo "\nUpdate unsuccessful";
+    return;
+  }
+}
 ?>
