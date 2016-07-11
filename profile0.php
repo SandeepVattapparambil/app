b62a6ef155d18e3c77f98dd12a3f496de9756dc2
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-07-11 11:47:51 +0530

    updates

--- profile0.php
+++ profile0.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['id']) && isset($_SESSION['user_name'])){
 //Require Database connection configuration file
 require_once('php/db_connection.php');
 mysqli_select_db($conn, $db_name);
-$sql = "SELECT * FROM user WHERE username='$user_name'";
+$sql = "SELECT * FROM user";
 $result = mysqli_query($conn, $sql);//or die('Query:<br />' . $sql . '<br /><br />Error:<br />' . mysql_error());
 $count_rows = mysqli_num_rows($result);
 ?>
